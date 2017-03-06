6edf12d9594448f15c97b350220d6c3dd060f909
Kahmali Rose <krose72205@gmail.com>
2017-03-05 19:48:40 -0500

    Add initial draft of landing page

--- client/pages/landing/landing.js
+++ client/pages/landing/landing.js
@@ -20,21 +20,21 @@ Template.LandingPage.onCreated(function() {
   this.phoneNumber = new ReactiveVar("");
 
   // Enable facebook "Message Us" plugin
-  window.fbAsyncInit = function() {
-    window.FB.init({
-      // TODO Add App ID from facebook dev console to Meteor settings
-      appId: "1729695877303019",
-      xfbml: true,
-      version: "v2.6"
-    });
-  };
-  (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+//   window.fbAsyncInit = function() {
+//     window.FB.init({
+//       // TODO Add App ID from facebook dev console to Meteor settings
+//       appId: "1729695877303019",
+//       xfbml: true,
+//       version: "v2.6"
+//     });
+//   };
+//   (function(d, s, id){
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) { return; }
+//     js = d.createElement(s); js.id = id;
+//     js.src = "//connect.facebook.net/en_US/sdk.js";
+//     fjs.parentNode.insertBefore(js, fjs);
+//   }(document, 'script', 'facebook-jssdk'));
 });
 
 Template.LandingPage.onRendered(function() {
