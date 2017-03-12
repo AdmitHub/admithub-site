577c42bfce35c9e388f93fc36e4fa7da4bdf0848
Kahmali Rose <krose72205@gmail.com>
2017-03-12 07:23:51 -0400

    Enable terms and privacy pages

--- client/pages/terms-of-service/terms-of-service.js
+++ client/pages/terms-of-service/terms-of-service.js
@@ -1,13 +1,13 @@
-// FlowRouter.route('/terms', {
-//   name: 'Terms',
-//
-//   meta: {
-//     pageTitle: 'Terms of Service'
-//   },
-//
-//   action: function() {
-//     BlazeLayout.render('Layout', {
-//       pageContent: 'TermsOfServicePage',
-//     });
-//   }
-// });
+FlowRouter.route('/terms', {
+  name: 'Terms',
+
+  meta: {
+    pageTitle: 'Terms of Service'
+  },
+
+  action: function() {
+    BlazeLayout.render('Layout', {
+      pageContent: 'TermsOfServicePage',
+    });
+  }
+});
