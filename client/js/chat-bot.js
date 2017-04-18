4acb66d9fc93b095591af0dc2501ef904870bc2c
jmp1548 <jmp1548@rit.edu>
2017-04-18 19:22:08 -0400

    Redo convo

--- client/js/chat-bot.js
+++ client/js/chat-bot.js
@@ -5,72 +5,63 @@ export function chatBot() {
       step2 = {
         'menu': [
           {
-            'title': 'Student or Parent',
+            'title': 'High School',
+            'startConclusion': 'true',
+            'conclusionnum': '2'
+          },
+          {
+            'title': 'College or University',
+            'startStep': true,
+            'stepnum': '3',
+          },
+          {
+            'title': 'I\'m A Student',
             'submenu': [
               {
-                'message': 'That’s great.  Let’s continue this conversation on Facebook, and I can help with every aspect of applying to college',
+                'message': 'No worries.  We can always chat on Facebook. Just click the button below... ',
                 'title': 'Facebook Messenger',
                 'href': 'https://www.messenger.com/t/admithub/',
               },
             ],
           },
           {
-            'title': 'Educator',
-            'startStep': true,
-            'stepnum': '3',
-          },
-          {
             'title': 'None of these',
+            'startConclusion': 'true',
+            'conclusionnum': '2',
           },
         ],
       },
       step3 = {
         'menu': [
           {
-            'title': 'College or University',
+            'title': 'Admission Officer',
             'startStep': true,
             'stepnum': '4',
           },
           {
-            'title': 'High School',
-            'submenu': [
-              {
-                'message': '&#x1f44d;',
-                'stepnum': '4',
-              },
-            ],
+            'title': 'Dean or VP',
+            'startStep': true,
+            'stepnum': '4',
           },
           {
-            'title': 'Other',
-            'submenu': [
-              {
-                'message': '&#x1f44d;',
-                'stepnum': '4',
-              },
-            ],
+            'title': 'Student Success',
+            'startStep': true,
+            'stepnum': '4',
+          },
+          {
+            'title': 'Something else',
+            'startStep': true,
+            'stepnum': '4',
           },
         ],
       },
       step4 = {
         'menu': [
           {
-            'title': 'Admission Officer',
+            'inputStep': true,
             'startStep': true,
-            'stepnum': '5',
-          },
-          {
-            'title': 'Dean or VP',
-            'startStep': true,
-            'stepnum': '5',
-          },
-          {
-            'title': 'Student Success',
-            'startStep': true,
-            'stepnum': '5',
-          },
-          {
-            'title': 'Something else',
-            'startStep': true,
+            'id': 'collegeName',
+            'placeholder': 'College Name',
             'stepnum': '5',
           },
         ],
@@ -80,7 +71,48 @@ export function chatBot() {
           {
             'inputStep': true,
             'startStep': true,
+            'id': 'email',
+            'placeholder': 'Email Address',
             'stepnum': '6',
+          },
+        ],
+      },
+      conclusion1 = {
+        'menu': [
+          {
+            'inputStep': true,
+            'id': 'collegeName',
+            'placeholder': 'College Name',
+            'startConclusion': true,
+            'conclusionnum': '2',
+          },
+        ],
+      },
+      conclusion2 = {
+        'menu': [
+          {
+            'title': 'Yes!',
+            'startConclusion': true,
+            'conclusionnum': '3',
+          },
+          {
+            'title': 'No thanks.',
+            'end': true,
+            'submenu': [
+              {
+                'message': 'Okay.  Feel free to come back if you change your mind.',
+              },
+            ],
+          },
+        ],
+      },
+      conclusion3 = {
+        'menu': [
+          {
+            'inputStep': true,
+            'id': 'email',
+            'placeholder': 'Email Address',
+            'end': true,
           },
         ],
       },
@@ -155,10 +187,10 @@ export function chatBot() {
       step = eval(`step${num}`),
       goBack = chat.querySelector('button.newmenu'),
       replies = [
-        'Which of these best describes who you are?',
-        'What best describes where you work?',
+        'And what best describes where you work?',
         'Which of these best describes your role?',
         'What institution do you work for?',
+        'Finally, what’s your email address?',
       ];
     if (goBack) {
       makeUserBubble(goBack);
@@ -170,7 +202,10 @@ export function chatBot() {
           menu += `<button class="choice menu" data-step="${num}" data-submenu="${index}">${val.title}</button>`;
         }
         else if (val.inputStep && val.startStep) {
-          menu += `<input type="text" id="collegeName" placeholder="College Name" /> <button data-step="${nextStep}" class="choice input startStep">Submit</button>`;
+          menu += `<input type="text" id="${val.id}" placeholder="${val.placeholder}" /> <button data-step="${nextStep}" class="choice input startStep">Submit</button>`;
+        }
+        else if (val.startConclusion) {
+          menu += `<button class="choice startConclusion" data-conclusion="${val.conclusionnum}">${val.title}</button>`;
         }
         else {
           menu += `<button class="choice startStep" data-step="${nextStep}">${val.title}</button>`;
@@ -185,7 +220,36 @@ export function chatBot() {
     }, idletime);
   };
 
-  const menuClick = (clicked, num) => {
+  const showConclusion = num => {
+    let menu = '',
+      nextConclusion = parseInt(num) + 1,
+      conclusion= eval(`conclusion${num}`),
+      goBack = chat.querySelector('button.newmenu');
+    if (goBack) {
+      makeUserBubble(goBack);
+    }
+    setTimeout(() => {
+      conclusion.menu.forEach((val, index) => {
+        if (val.submenu) {
+          menu += `<button class="choice menu" data-conclusion="${num}" data-submenu="${index}">${val.title}</button>`;
+        }
+        else if (val.inputStep) {
+          menu += `<input type="text" id="${val.id}" placeholder="${val.placeholder}" /> <button data-conclusion="${nextConclusion}" class="choice startConclusion input">Submit</button>`;
+        }
+        else {
+          menu += `<button class="choice startConclusion" data-conclusion="${nextConclusion}">${val.title}</button>`;
+        }
+      });
+      setTimeout(() => {
+        newMessage(menu);
+      }, 300);
+    }, 500);
+    idle = window.setInterval(() => {
+      window.clearInterval(idle);
+    }, idletime);
+  };
+
+  const stepMenuClick = (clicked, num) => {
     let submenu = '',
       message = '',
       step = eval(`step${num}`),
@@ -195,10 +259,6 @@ export function chatBot() {
       if (val.href && val.title) {
         submenu += `<a href="${val.href}"><button class="submenu">${val.title}</button></a>`;
       }
-      // else if (val.startStep) {
-      //   submenu += `<button data-step="${val.stepnum}" class="choice startStep">${val.title}</button>`;
-      // }
-
       message += val.message;
 
     });
@@ -253,11 +313,31 @@ export function chatBot() {
       window.clearInterval(idle);
       if (!e.target.classList.contains('submenu')) {
         if (e.target.classList.contains('input')) {
+          const parentHTML = e.target.parentNode.innerHTML;
+          const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
           const input = e.target.parentNode.getElementsByTagName('input')[0];
           const inputId = input.getAttribute('id');
+          const emailInput = document.getElementById('email');
 
-          data[inputId] = input.value;
           makeUserBubble(input);
+
+          if (input.id === 'email' && !input.value.match(emailRegex)) {
+            newMessage('Pleae enter a valid email','bot');
+
+            newMessage(parentHTML);
+            return;
+          }
+
+          if (input.value === '') {
+
+            newMessage(`Pleae enter your ${input.placeholder.toLowerCase()}`,'bot');
+
+            newMessage(parentHTML);
+            return;
+          }
+          else {
+            data[inputId] = input.value;
+          }
         } else {
           makeUserBubble(e.target);
         }
@@ -265,76 +345,78 @@ export function chatBot() {
 
       if (e.target.classList.contains('menu')) {
         const stepNum = e.target.getAttribute('data-step');
-        menuClick(e.target, stepNum);
+        stepMenuClick(e.target, stepNum);
       }
-
-      // if (e.target.classList.contains('submenu')) {
-      //   subMenuClick(e.target);
-      // }
 
       if (e.target.classList.contains('startStep')) {
         const stepNum = e.target.getAttribute('data-step');
 
-        if (stepNum === '2') {
-          newMessage(`Nice to meet you ${data["firstName"]}`, 'bot');
-          showMenu(stepNum);
-        }
-
-        if (stepNum === '3') {
-          newMessage('Okay great!', 'bot');
-          showMenu(stepNum);
-        }
-
-        if (stepNum === '4') {
-          setTimeout(() => {
-            newMessage('&#x1f44d;', 'bot');
+        switch (stepNum) {
+          case '2':
             setTimeout(() => {
-              newMessage('[problems we solve]', 'bot');
+              newMessage(`Nice to meet you ${data["firstName"]}`, 'bot');
               setTimeout(() => {
-                showMenu(stepNum);
+                newMessage('[one sentence about what we do…]', 'bot');
+                setTimeout(() => {
+                  showMenu(stepNum);
+                }, 300);
               }, 300);
-            }, 300);
-          }, 500);
-        }
+            }, 500);
+            break;
 
-        if (stepNum === '5') {
-          newMessage(' [benefit we bring]', 'bot');
-          showMenu(stepNum);
-        }
+          case '3':
+            setTimeout(() => {
+              newMessage('Okay great!', 'bot');
+              setTimeout(() => {
+                newMessage('[describe the problems we solve]', 'bot');
+                setTimeout(() => {
+                  showMenu(stepNum);
+                }, 300);
+              }, 300);
+            }, 500);
 
-        if (stepNum === '6') {
-          newMessage(`Does ${data["collegeName"]}....`, 'bot');
-          showMenu(stepNum);
+            break;
+
+          case '4':
+            newMessage('[benefit we bring]', 'bot');
+            showMenu(stepNum);
+            break;
+
+          case '5':
+            newMessage(`Does ${data["collegeName"]}....`, 'bot');
+            showMenu(stepNum);
+            break;
+
+          case '6':
+            newMessage('Thanks, you rock!', 'bot');
+            break;
+
+          }
         }
       }
 
-      // if (e.target.classList.contains('showmenu')) {
-      //   showMenu();
-      // }
+      if (e.target.classList.contains('startConclusion')) {
+        const conclusionNum = e.target.getAttribute('data-conclusion');
 
-      // if (e.target.classList.contains('showinfo')) {
-      //   let infoReplies = [
-      //       'Here\'s my website ',
-      //       'Here you go: ',
-      //       'Check this one out '
-      //     ],
-      //     okReplies = [
-      //       'OK &#x1F60E;',
-      //       'How do I get back? &#x1F312;',
-      //       'Ok, thanks! &#x1F44C;'
-      //     ];
-      //   setTimeout(() => {
-      //     newMessage(`${randomReply(infoReplies)} <a target="_new" href="https://librarian.codes">https://librarian.codes</a>`, 'bot');
-      //     setTimeout(() => {
-      //       newMessage(`<button class="choice newmenu showmenu">${randomReply(okReplies)}</button>`);
-      //     }, 300);
-      //   }, 500);
-      // }
+        switch (conclusionNum) {
+          case '1':
+            newMessage('What’s the name of your school or organization?', 'bot');
+            showConclusion(conclusionNum);
+            break;
+
+          case '2':
+            newMessage('Do you want to sign-up for our newsletter to get the latest and greatest news about AdmitHub?', 'bot');
+            showConclusion(conclusionNum);
+            break;
+
+          case '3':
+            newMessage('What’s your email address?', 'bot');
+            showConclusion(conclusionNum);
+            break;
+        }
+      }
     }
-    if (e.target.classList.contains('close')) {
-      toggleContent();
-    }
-  });
+  );
 
   window.addEventListener('popstate', () => {
     toggleContent();
