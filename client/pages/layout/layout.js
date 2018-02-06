390a4952a152019cd4b5683adf4ad8df21cc90e0
Andrew Magliozzi <andrew.magliozzi@gmail.com>
2018-02-05 20:28:29 -0500

    hiding div

--- client/pages/layout/layout.js
+++ client/pages/layout/layout.js
@@ -11,6 +11,13 @@ Template.Layout.onCreated(function() {
         version: 'v2.12' // Specify your version here
       });
     };
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
   }
 
   // Hide the address bar on mobile devices (see https://davidwalsh.name/hide-address-bar)
