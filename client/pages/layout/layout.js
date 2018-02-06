b21866176ee1874f4ffcc0d4974ea1414e980c5c
Andrew Magliozzi <andrew.magliozzi@gmail.com>
2018-02-05 20:46:32 -0500

    hiding iframe

--- client/pages/layout/layout.js
+++ client/pages/layout/layout.js
@@ -18,6 +18,7 @@ Template.Layout.onCreated(function() {
        js.src = "https://connect.facebook.net/en_US/sdk.js";
        fjs.parentNode.insertBefore(js, fjs);
      }(document, 'script', 'facebook-jssdk'));
+    $("iframe").hide();
   }
 
   // Hide the address bar on mobile devices (see https://davidwalsh.name/hide-address-bar)
