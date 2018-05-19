b5efe5caac3b466d24aac97904796c6d66522ad2
Timothy Moliterno <timothy.moliterno@gmail.com>
2018-05-19 19:01:46 -0400

    Adds Google Tag Manager

--- server/api/analytics.js
+++ server/api/analytics.js
@@ -56,3 +56,25 @@ function authenticateTwilio(request) {
     return valid;
   }
 }
+
+/**
+ * Injects GTM script and iframe directly into top of head and body before loading meteor packages
+ * 
+ */
+
+Inject.rawHead('headGA', `
+<!-- Google Tag Manager -->  
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-P7LXZX');</script>
+<!-- End Google Tag Manager -->
+`);
+
+Inject.rawBody('bodyGA', `
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P7LXZX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+`);
