ecff3cbb6d4134b3925f0243e9e1e91a75ebf754
Timothy Moliterno <timothy.moliterno@gmail.com>
2018-06-11 18:37:21 -0400

    Removes duplicate pixel

--- server/api/analytics.js
+++ server/api/analytics.js
@@ -70,22 +70,6 @@ Inject.rawHead('headGA', `
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','GTM-P7LXZX');</script>
 <!-- End Google Tag Manager -->
-<!-- Facebook Pixel -->
-<script>
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '197227914338060');
-  fbq('track', 'BotStart');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=197227914338060&ev=PageView&noscript=1"
-/></noscript>
 `);
 
 Inject.rawBody('bodyGA', `
