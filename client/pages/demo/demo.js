b3e43f332451d53db9bc80c40c864c0dd2cb6ba0
Timothy Moliterno <timothy.moliterno@gmail.com>
2018-07-26 11:41:51 -0400

    Add hidden demo path

---
+++ client/pages/demo/demo.js
@@ -0,0 +1,13 @@
+FlowRouter.route('/demo', {
+    name: 'Demo',
+  
+    meta: {
+      pageTitle: 'Georgia State Demo'
+    },
+  
+    action: function() {
+      BlazeLayout.render('Layout', {
+        pageContent: 'GSUDemo',
+      });
+    }
+  });
